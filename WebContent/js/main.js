dbaec5128b283320086b231a9b71777c9e03ef27
lucas_reduzino <lucas_reduzino@SNFNS2EP13VM019.florianopolis.sc.senai.br>
2014-07-02 21:31:27 -0300

    formulario

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,3 +1,51 @@
 /**
  * 
  */
+document.write('<h1>Formulario</h1>');
+
+
+tabela = document.createElement('table');
+trh = document.createElement('tr');
+th = document.createElement('th');
+th.innerText = 'Nome';
+
+
+tabela.appendChild(trh).appendChild(th);
+
+formulario = document.createElement('form');
+
+nome = document.createElement('input');
+nome.setAttribute('placeholder', 'Digite o nome');
+
+idade = document.createElement('input');
+idade.setAttribute('placeholder', 'Digite a idade');
+
+sexo = document.createElement('input');
+sexo.setAttribute('placeholder', 'sexo');
+
+function adicionar(evento){
+	evento.preventDefault();
+	var tr = document.createElement('tr');
+	var td = document.createElement('td');
+	td.innerText = nome.value;
+	
+	
+	nome.value = '';
+
+
+	tabela.appendChild(tr).appendChild(td);
+}
+
+botaoSalvar = document.createElement('button');
+botaoSalvar.innerText = 'Adicionar';
+botaoSalvar.addEventListener('click', adicionar);
+
+
+formulario.appendChild(nome);
+formulario.appendChild(idade);
+formulario.appendChild(sexo);
+formulario.appendChild(botaoSalvar);
+document.body.appendChild(formulario);
+document.body.appendChild(tabela);
+
+
